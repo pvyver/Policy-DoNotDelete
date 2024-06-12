4f49e4704beb3af581c041b4f0b780a80b3ee7ed
Philip Van de Vyver <pvyver@yahoo.com>
2024-06-12 13:18:44 +0200

    remove subid

--- assignPolicy.ps1
+++ assignPolicy.ps1
@@ -1,18 +1,16 @@
-# $policyDefinitionId = "78460a36-508a-49a4-b2b2-2f5ec564f4bb"
-$subscriptionId = "/subscriptions/73d1976a-3d59-4c49-948e-847f08f43071"
+$subscriptionId = "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 
+#region policy definition
 $polDefName = "do-not-allow-deletion"
 $polDefDisplayName = "Do not allow deletion of resources"
-$polAssName = "do-not-allow-deletion"
-$polAssDisplayName  = "Do not allow deletion of resources"
-
 
 New-AzPolicyDefinition -Name $polDefName -DisplayName $polDefDisplayName -Policy ".\policies\doNotAllowDeletionPolicy.json"
+#region policy definition
 
-$listOfResourceTypesDisallowedForDeletion = Get-Content .\allowedResourceTypesForDeletion.txt
-$policyParameterObject = @{
-    'listOfResourceTypesAllowedForDeletion' = $listOfResourceTypesDisallowedForDeletion
-}
+
+#region policy assignment
+$polAssName = "do-not-allow-deletion"
+$polAssDisplayName  = "Do not allow deletion of resources"
 
 $policyParameter = @"
 {
@@ -26,12 +24,12 @@ $policyParameter = @"
 }
 "@
 
-
 $policy = Get-AzPolicyDefinition -Custom | Where-Object {$_.name -eq "do-not-allow-deletion"}
  
 New-AzPolicyAssignment -Name $polAssName -Scope $subscriptionId `
  -DisplayName $polAssDisplayName -PolicyDefinition $policy `
  -PolicyParameter $policyParameter
 
+#endregion policy assignment
 
  
